2aa467548dcac3f3ce44282980b8cdf475e05823
Reymor <Reymor@list.ru>
2017-08-19 00:11:09 +0300

    add config webpack for normal

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
-
 module.exports = {
     entry: path.resolve(__dirname, './src/markdown-html-parser.js'),
 
     output: {
         path: path.resolve(__dirname, './lib'),
-        filename: 'markdown-html-parser.min.js',
-	    library: 'markdown-html-parser.min',
+        filename: 'markdown-html-parser.js',
+	    library: 'markdown-html-parser',
 	    libraryTarget: 'umd'
     },
 
@@ -16,19 +15,6 @@ module.exports = {
             test: /\.js$/,
             loaders: 'babel-loader',
         }]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {warnings: false},
-            output: {comments: false},
-            comments: false
-        })
-    ],
+    }
 };
 
